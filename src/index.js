784383b6569200672de6657374827df8f11fa840
sahaphop <sahaphop@dollarsmart.co>
2019-01-31 14:04:02 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+// 1
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+
+// 2
+const resolvers = {
+  Query: {
+    info: () => 'This is the API of a Hackernews Clone',
+    feed: () => links,
+  },
+  Mutation: {
+    post: (parent, args) => {
+       const link = {
+        id: `link-${idCount++}`,
+        description: args.description,
+        url: args.url,
+      }
+      links.push(link)
+      return link
+    }
+  }
+}
+
+// 3
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
